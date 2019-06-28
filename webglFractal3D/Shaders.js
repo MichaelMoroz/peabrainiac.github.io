1c19725dfffc111b84ebcc5a558890419902e54f
Moroz Michael <47035925+MichaelMoroz@users.noreply.github.com>
2019-06-29 01:51:08 +0300

    mandelbulb DE

--- webglFractal3D/Shaders.js
+++ webglFractal3D/Shaders.js
@@ -82,8 +82,34 @@ const Shaders = (function(){
 				}
 			}
 			
+
+			float DE(vec3 pos) {
+				vec3 z = pos;
+				float dr = 1.0;
+				float r = 0.0;
+				for (int i = 0; i < Iterations ; i++) {
+					r = length(z);
+					if (r>Bailout) break;
+
+					// convert to polar coordinates
+					float theta = acos(z.z/r);
+					float phi = atan(z.y,z.x);
+					dr =  pow( r, Power-1.0)*Power*dr + 1.0;
+
+					// scale and rotate the point
+					float zr = pow( r,Power);
+					theta = theta*Power;
+					phi = phi*Power;
+
+					// convert back to cartesian coordinates
+					z = zr*vec3(sin(theta)*cos(phi), sin(phi)*sin(theta), cos(theta));
+					z+=pos;
+				}
+				return 0.5*log(r)*r/dr;
+			}
+
 			float dst_scene(vec3 pos){
-				float det = pow(determinant(fractalTransformation1),0.33333);
+				/*float det = pow(determinant(fractalTransformation1),0.33333);
 				float factor = 1.0;
 				vec3 p = pos;
 				float sphereRad = 1.0;
@@ -97,9 +123,11 @@ const Shaders = (function(){
 						break;
 					}
 				}
-				return (length(p)-sphereRad)/factor;
+				return (length(p)-sphereRad)/factor;*/
+				return DE(pos);
 			}
 			
+
 			float surfaceAngle(vec3 pos, vec3 direction){
 				float det = pow(determinant(fractalTransformation1),0.33333);
 				vec3 p = pos;
